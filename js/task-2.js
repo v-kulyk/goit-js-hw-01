aca45658b333e7fafdc123e28abb1a7ad3c7739b
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-05-05 14:14:50 +0300

    format code with prettier

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
+  const totalPrice = price + deliveryFee;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
 console.log(getShippingMessage("Australia", 120, 50));
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
 
-    return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  return contentWidth + 2 * paddingWidth + 2 * borderWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
